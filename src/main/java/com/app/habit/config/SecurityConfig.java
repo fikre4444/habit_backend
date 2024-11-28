1ce8f1984ea0764fa9dc30d527d803dac608c3c3
fikre4444 <fikretesfay4444@gmail.com>
2024-11-29 00:53:19 +0300

    finally made some progress with oauth2

--- src/main/java/com/app/habit/config/SecurityConfig.java
+++ src/main/java/com/app/habit/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.app.habit.config;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +11,19 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.app.habit.filter.JwtFilter;
 import com.app.habit.service.MyUserDetailsService;
+import com.app.habit.service.OAuth2LoginSuccessHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 public class SecurityConfig {
@@ -26,18 +34,36 @@ public class SecurityConfig {
   @Autowired
   private JwtFilter jwtFilter;
 
+  @Autowired
+  private OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http = http.csrf(customzer -> customzer.disable())
         .cors(customizer -> customizer.disable())
         .httpBasic(customizer -> customizer.disable())
         .authorizeHttpRequests(customizer -> customizer
-            .requestMatchers("/api/hello/**", "/api/auth/**", "/api/register/**", "/h2-console/**").permitAll()
+            .requestMatchers("/api/hello/**", "/api/auth/**", "/api/register/**", "/h2-console/**", "/api/user/welcome")
+            .permitAll()
             .requestMatchers("/api/admin/**").hasRole("ADMINISTRATOR")
             .requestMatchers("/api/user/**").hasRole("USER"))
-        .headers(customizer -> customizer.frameOptions(customizer1 -> customizer1.sameOrigin()))
+        .headers(customizer -> customizer.frameOptions(customizer1 -> customizer1.sameOrigin())) // for h2
         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .exceptionHandling(exception -> exception
+            .authenticationEntryPoint(new AuthenticationEntryPoint() {
+              @Override
+              public void commence(HttpServletRequest request, HttpServletResponse response,
+                  AuthenticationException authException) throws IOException, ServletException {
+                response.setContentType("application/json");
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter()
+                    .write("{\"error\": \"Unauthorized\", \"message\": \"" + authException.getMessage() + "\"}");
+              }
+            }))
+        .oauth2Login(oauth2 -> oauth2
+            .loginPage("/oauth2/authorization/google") // Ensure this matches your setup
+            .successHandler(oAuth2LoginSuccessHandler)); // Success handler
 
     return http.build();
   }
@@ -48,11 +74,6 @@ public class SecurityConfig {
     dao.setUserDetailsService(userDetailsService);
     dao.setPasswordEncoder(new BCryptPasswordEncoder());
     return dao;
-  }
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
   }
 
   @Bean
--- src/main/java/com/app/habit/repository/UserRepository.java
+++ src/main/java/com/app/habit/repository/UserRepository.java
@@ -8,4 +8,7 @@ import com.app.habit.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByUsername(String username);
+
+  Optional<User> findByEmail(String email);
+
 }
