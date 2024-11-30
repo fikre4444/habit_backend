6fe8acb2e553cbbb108b4adb91e4bb9c8c900045
fikre4444 <fikretesfay4444@gmail.com>
2024-11-30 09:05:50 +0300

    fixed the cors problem

--- src/main/java/com/app/habit/config/SecurityConfig.java
+++ src/main/java/com/app/habit/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -16,13 +17,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.app.habit.filter.JwtFilter;
+import com.app.habit.handler.CustomAuthenticationEntryPoint;
 import com.app.habit.service.MyUserDetailsService;
 import com.app.habit.service.OAuth2LoginSuccessHandler;
 
@@ -41,6 +40,9 @@ public class SecurityConfig {
 
   @Autowired
   private OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+
+  @Autowired
+  private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
   @Bean
   public WebMvcConfigurer corsConfigurer() {
@@ -61,6 +63,7 @@ public class SecurityConfig {
     http = http.csrf(customzer -> customzer.disable())
         .httpBasic(customizer -> customizer.disable())
         .authorizeHttpRequests(customizer -> customizer
+            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
             .requestMatchers("/api/hello/**", "/api/auth/**", "/api/register/**", "/h2-console/**", "/api/user/welcome")
             .permitAll()
             .requestMatchers("/api/admin/**").hasRole("ADMINISTRATOR")
@@ -69,16 +72,7 @@ public class SecurityConfig {
         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .exceptionHandling(exception -> exception
-            .authenticationEntryPoint(new AuthenticationEntryPoint() {
-              @Override
-              public void commence(HttpServletRequest request, HttpServletResponse response,
-                  AuthenticationException authException) throws IOException, ServletException {
-                response.setContentType("application/json");
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                response.getWriter()
-                    .write("{\"error\": \"Unauthorized\", \"message\": \"" + authException.getMessage() + "\"}");
-              }
-            }))
+            .authenticationEntryPoint(customAuthenticationEntryPoint))
         .oauth2Login(oauth2 -> oauth2
             .loginPage("/oauth2/authorization/google") // Ensure this matches your setup
             .successHandler(oAuth2LoginSuccessHandler)); // Success handler
